6470719adac7c9a8c61f0d50b308fdda46d6fdf0
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-03-08 20:26:08 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Gameboard from './components/Gameboard';
+import './App.css';
+
+function App() {
+  return (
+    <Gameboard/>
+  );
+}
+
+export default App;
---
+++ src/components/Gameboard.js
@@ -0,0 +1,17 @@
+import './styles/gameboard.css';
+
+export default function Gameboard(){
+    return(
+        <div class="game-board">
+            <div class="box">200px x 200px</div>
+            <div class="box">200px x 200px</div>
+            <div class="box">200px x 200px</div>
+            <div class="box">200px x 200px</div>
+            <div class="box">200px x 200px</div>
+            <div class="box">200px x 200px</div>
+            <div class="box">200px x 200px</div>
+            <div class="box">200px x 200px</div>
+            <div class="box">200px x 200px</div>
+        </div>
+    );
+}
