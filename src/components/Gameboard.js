cfb39ceda264e3b3a7a4668c93c692e7d28efa7a
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-03-10 22:02:00 -0700

    Upd

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -4,14 +4,14 @@ export default function Gameboard(props){
     return(
         <div class="game-board">
             <div class="box">{props.gameArray[0]}</div>
-            <div class="box"></div>
-            <div class="box"></div>
-            <div class="box"></div>
-            <div class="box"></div>
-            <div class="box"></div>
-            <div class="box"></div>
-            <div class="box"></div>
-            <div class="box"></div>
+            <div class="box">{props.gameArray[1]}</div>
+            <div class="box">{props.gameArray[2]}</div>
+            <div class="box">{props.gameArray[3]}</div>
+            <div class="box">{props.gameArray[4]}</div>
+            <div class="box">{props.gameArray[5]}</div>
+            <div class="box">{props.gameArray[6]}</div>
+            <div class="box">{props.gameArray[7]}</div>
+            <div class="box">{props.gameArray[8]}</div>
         </div>
     );
 }
