01157c11e29e2e4cd5f26633ef29d0c5272a7aba
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-03-10 23:16:55 -0700

    Game array

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,17 +1,12 @@
 import './styles/gameboard.css';
 
 export default function Gameboard(props){
+    const arrayElemets = props.gameArray;
+    const ticItems = arrayElemets.map( x => <div class="box"> x </div> );
+    
     return(
         <div class="game-board">
-            <div class="box">{props.gameArray[0]}</div>
-            <div class="box">{props.gameArray[1]}</div>
-            <div class="box">{props.gameArray[2]}</div>
-            <div class="box">{props.gameArray[3]}</div>
-            <div class="box">{props.gameArray[4]}</div>
-            <div class="box">{props.gameArray[5]}</div>
-            <div class="box">{props.gameArray[6]}</div>
-            <div class="box">{props.gameArray[7]}</div>
-            <div class="box">{props.gameArray[8]}</div>
+            {ticItems}
         </div>
     );
 }
