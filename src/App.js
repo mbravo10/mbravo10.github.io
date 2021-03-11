b2676bdbe2acbe7ed6a2b96e071a61ef603ba2c6
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-03-10 21:57:56 -0700

    Adding gameboard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Gameboard from './components/Gameboard';
 import './App.css';
 
+const SELECTIONS = [1, 2, 3,
+                    4, 5, 6,
+                    7, 8, 9];
+
 function App() {
   return (
-    <Gameboard/>
+    <Gameboard gameArray={SELECTIONS}/>
   );
 }
 
--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,17 +1,17 @@
 import './styles/gameboard.css';
 
-export default function Gameboard(){
+export default function Gameboard(props){
     return(
         <div class="game-board">
-            <div class="box">200px x 200px</div>
-            <div class="box">200px x 200px</div>
-            <div class="box">200px x 200px</div>
-            <div class="box">200px x 200px</div>
-            <div class="box">200px x 200px</div>
-            <div class="box">200px x 200px</div>
-            <div class="box">200px x 200px</div>
-            <div class="box">200px x 200px</div>
-            <div class="box">200px x 200px</div>
+            <div class="box">{props.gameArray[0]}</div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
         </div>
     );
 }
