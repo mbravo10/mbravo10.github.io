6ade82f8f1328debd0ad0d2dcf3197e9c6b7b5b0
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-03-12 21:08:40 -0700

    Added container for flexbox adjustments

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,12 +1,28 @@
 import './styles/gameboard.css';
+import { Container, Jumbotron, Button, Row }from 'react-bootstrap';
+
 
 export default function Gameboard(props){
     const arrayElemets = props.gameArray;
-    const ticItems = arrayElemets.map( x => <div class="box"> x </div> );
+    const ticItems = arrayElemets.map( x => <div class="box"> {x} </div> );
     
     return(
-        <div class="game-board">
-            {ticItems}
-        </div>
+        <Container>
+            <Jumbotron>
+                <h1>Hello, world!</h1>
+                <p>
+                    This is a simple hero unit, a simple jumbotron-style component for calling
+                    extra attention to featured content or information.
+                </p>
+                <p>
+                    <Button variant="primary">Learn more</Button>
+                </p>
+            </Jumbotron>
+            <Row className="justify-content-md-center">
+                <div class="game-board">
+                    {ticItems}
+                </div>
+            </Row>
+        </Container>
     );
 }
