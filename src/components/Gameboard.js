cc30b83bfebe483abba57dcb707dbf8865aea7f3
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-03-15 20:16:52 -0600

    Added game status

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -2,6 +2,7 @@ import "./styles/gameboard.css";
 import { Container, Jumbotron, Button, Row, Col } from "react-bootstrap";
 import React from "react";
 import Square from "./Square";
+import Gamestatus from "./Gamestatus";
 
 export default class Gameboard extends React.Component {
   constructor(props) {
@@ -31,23 +32,26 @@ export default class Gameboard extends React.Component {
   render() {
     return (
       <Container fluid="true">
-        <Jumbotron>
+        <Jumbotron style={{ backgroundColor: "#0B0C10", color: "#45A29E" }}>
           <h1>Hello, world!</h1>
           <p>
             This is a simple hero unit, a simple jumbotron-style component for
             calling extra attention to featured content or information.
           </p>
           <p>
-            <Button variant="primary">Learn more</Button>
+            <Button style={{ background: "#45A29E" }}>Learn more</Button>
           </p>
         </Jumbotron>
         <Row className="justify-content-md-center">
-          <Col md="auto">
-            <div class="game-board">
+          <Col sm={{ span: 5, offset: 2 }}>
+            <div className="game-board">
               {this.props.gameArray.map((x, index) =>
                 this.renderSquare(x, index)
               )}
             </div>
+          </Col>
+          <Col>
+            <Gamestatus currentP={this.state.isXsTurn} />
           </Col>
         </Row>
       </Container>
---
+++ src/components/Gamestatus.js
@@ -0,0 +1,15 @@
+import { Badge, OverlayTrigger, Tooltip } from "react-bootstrap";
+
+export default function Gamestatus(props) {
+  return (
+    <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">Play!</Tooltip>}>
+      <span className="d-inline-block">
+        <h1>
+          <Badge variant="info">
+            {`Current player: ${props.currentP ? "X" : "O"}`}
+          </Badge>
+        </h1>
+      </span>
+    </OverlayTrigger>
+  );
+}
