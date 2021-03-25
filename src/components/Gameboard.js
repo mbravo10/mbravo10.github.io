4ea26e3cdcef8bc1fb0169abe8b1f58172dbe9d8
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-03-24 21:32:58 -0600

    Changed theme of application

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,4 +1,12 @@
-import { Container, Jumbotron, Button, Row, Col } from "react-bootstrap";
+import {
+  Container,
+  Jumbotron,
+  Button,
+  Row,
+  Col,
+  Card,
+  Toast,
+} from "react-bootstrap";
 import React from "react";
 import Square from "./Square";
 import Gamestatus from "./Gamestatus";
@@ -15,8 +23,9 @@ export default class Gameboard extends React.Component {
       isXsTurn: true,
       isDisabled: false,
       isLoading: false,
+      xWins: 0,
+      oWins: 0,
     };
-
     this.baseState = this.state;
   }
 
@@ -27,6 +36,15 @@ export default class Gameboard extends React.Component {
           {this.props.gameArray.map((x, index) => this.renderSquare(x, index))}
         </div>
       );
+    } else if (
+      prevState.xWins !== this.state.xWins ||
+      prevState.oWins !== this.state.oWins
+    ) {
+      return (
+        <Toast>
+          <Toast.Body>{`Wins for Player X: ${this.state.xWins}`}</Toast.Body>
+        </Toast>
+      );
     }
   }
 
@@ -34,15 +52,16 @@ export default class Gameboard extends React.Component {
     this.setState(this.baseState);
   }
 
-  simulateNetworkResponse() {
-    return new Promise((resolve) => setTimeout(resolve, 1000));
-  }
-
   handleClick(i) {
     const square = this.state.value.slice();
     this.state.isXsTurn ? (square[i] = "X") : (square[i] = "O");
     this.setState({ value: square, isXsTurn: !this.state.isXsTurn });
-    if (Winner(square)) this.setState({ isDisabled: true });
+    if (Winner(square)) {
+      this.setState({ isDisabled: true });
+      this.state.isXsTurn
+        ? this.setState({ xWins: this.state.xWins + 1 })
+        : this.setState({ oWins: this.state.oWins + 1 });
+    }
   }
 
   renderSquare(i, index) {
@@ -69,14 +88,19 @@ export default class Gameboard extends React.Component {
               boxShadow: "5px 20px 30px #1f2833",
             }}
           >
-            <h1>Hello, world!</h1>
-            <p>
-              This is a simple hero unit, a simple jumbotron-style component for
-              calling extra attention to featured content or information.
-            </p>
-            <p>
-              <Button>Learn more</Button>
-            </p>
+            <h1>Tic-Tac-Toe!</h1>
+            <Row>
+              <Col>
+                <Toast>
+                  <Toast.Body>{`Wins for Player X: ${this.state.xWins}`}</Toast.Body>
+                </Toast>
+              </Col>
+              <Col>
+                <Toast>
+                  <Toast.Body>{`Wins for Player O: ${this.state.oWins}`}</Toast.Body>
+                </Toast>
+              </Col>
+            </Row>
           </Jumbotron>
           <Row className="justify-content-md-center">
             <Row>
@@ -88,9 +112,18 @@ export default class Gameboard extends React.Component {
                 </div>
               </Col>
               <Col>
-                <Gamestatus currentP={this.state.isXsTurn} />
-                <Gamewinner values={this.state.value} />
-                <Resetbutton onClick={() => this.handleReset()} />
+                <Card
+                  style={{
+                    backgroundColor: "#1f2933",
+                    boxShadow: "5px 20px 30px #1f2833",
+                  }}
+                >
+                  <Card.Body>
+                    <Gamestatus currentP={this.state.isXsTurn} />
+                    <Gamewinner values={this.state.value} />
+                    <Resetbutton onClick={() => this.handleReset()} />
+                  </Card.Body>
+                </Card>
               </Col>
             </Row>
           </Row>
