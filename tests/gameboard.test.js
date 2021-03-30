f9303529b4a105dda1c3e7fddec02b12a1c3c61a
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-03-29 18:23:30 -0600

    Added tests

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,17 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { act } from "react-dom/test-utils";
-import Gameboard from "../src/components/Gameboard";
-
+const puppeteer = require("puppeteer");
+let browser;
+let page;
 // Note: running cleanup afterEach is done automatically for you in @testing-library/react@9.0.0 or higher
 // unmount and cleanup DOM after the test is finished.
-let container;
-it("CheckboxWithLabel changes the text after click", () => {
-  act(() => {
-    ReactDOM.render(<Gameboard />, container);
-  });
+async function openBrowser() {
+  browser = await puppeteer.launch();
+  page = await browser.newPage();
+  await page.goto("https://mbravo10.github.io");
+}
+test("H1 Title has correct text", async () => {
+  await openBrowser();
+  // other actions...
+  let text = await page.$eval("h1", (el) => el.textContent);
+  expect(text).toBe("Tic-Tac-Toe!");
+});
 
-  const h1 = container.querySelector("h1");
+test("Player displays 0", async () => {
+  // other actions...
+  var res = await page.$$eval(".toast-body", (el) =>
+    el.map((x) => x.textContent)
+  );
+  expect(res[0]).toBe("Player ❌ wins : 0");
+  expect(res[1]).toBe("Player ⭕️ wins : 0");
 
-  expect(h1.tezt).toBe("Tic-Tac-Toe!");
+  page.close();
 });
